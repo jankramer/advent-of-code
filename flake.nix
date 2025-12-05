10d1f20f7ab05fd6d3769b92d537b9351519e1dd
Jan Kramer <jan@jankramer.eu>
2025-12-05 11:06:28 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Advent of Code";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      overlays = [ (import rust-overlay) ];
+      mkPkgs = system: import nixpkgs { inherit system overlays; };
+
+      mkShellForSystem =
+        system:
+        with (mkPkgs system);
+        mkShell {
+          buildInputs = [
+            (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
+            nixfmt-rfc-style
+          ];
+        };
+    in
+    {
+      devShells.aarch64-darwin.default = mkShellForSystem "aarch64-darwin";
+      devShells.x86_64-linux.default = mkShellForSystem "x86_64-linux";
+    };
+}
