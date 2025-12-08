d41a030aa4c853b7d4493b4add4d43b64c18fd06
Jan Kramer <jan@jankramer.eu>
2025-12-08 05:53:18 +0100

    2025 day 7: simplify

--- 2025/src/bin/d07.rs
+++ 2025/src/bin/d07.rs
@@ -1,51 +1,24 @@
-use std::collections::{HashMap, HashSet};
-
 fn run(input: &[u8]) -> (usize, usize) {
+    let mut p1 = 0;
     let width = input.iter().position(|&c| c == b'\n').unwrap() + 1;
-    let height = input.len() / width;
     let start = input.iter().position(|&c| c == b'S').unwrap();
 
-    let mut graph: HashMap<usize, HashSet<usize>> = Default::default();
-    let mut queue = vec![start];
-    let mut visited = HashSet::new();
+    let mut counters = vec![0; width];
+    counters[start] = 1;
 
-    while let Some(node) = queue.pop() {
-        let entry = graph.entry(node).or_default();
+    for line in input.chunks(width).step_by(2) {
+        for (index, byte) in line.iter().enumerate() {
+            if matches!(byte, b'^') && counters[index] > 0 {
+                counters[index - 1] += counters[index];
+                counters[index + 1] += counters[index];
+                counters[index] = 0;
 
-        for next in (1..(height - node / width)).map(|dy| node + width * dy) {
-            if input.get(next).map(|&b| b == b'^').unwrap_or(false) {
-                let (l, r) = (next - 1, next + 1);
-
-                entry.insert(l).then(|| queue.push(l));
-                entry.insert(r).then(|| queue.push(r));
-                visited.insert(next);
-
-                break;
+                p1 += 1;
             }
         }
     }
 
-    (visited.len(), paths(&mut Default::default(), &graph, start))
-}
-
-fn paths(
-    cache: &mut HashMap<usize, usize>,
-    graph: &HashMap<usize, HashSet<usize>>,
-    start: usize,
-) -> usize {
-    if cache.contains_key(&start) {
-        return *cache.get(&start).unwrap();
-    }
-
-    let nbs = graph.get(&start).unwrap();
-    if nbs.is_empty() {
-        return 1;
-    }
-
-    let result = nbs.iter().map(|nb| paths(cache, graph, *nb)).sum();
-    cache.insert(start, result);
-
-    result
+    (p1, counters.into_iter().sum())
 }
 
 fn main() {
