4cf4062a9e0e075ac570a01000452716f9142f12
Jan Kramer <jan@jankramer.eu>
2025-12-08 16:49:05 +0100

    2025 day 8: coords => indices

--- 2025/src/bin/d08.rs
+++ 2025/src/bin/d08.rs
@@ -1,5 +1,6 @@
 use std::cmp::Reverse;
 use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::hash::{BuildHasherDefault, Hasher};
 
 fn run(input: &str, n: usize) -> (usize, usize) {
     let coords: Vec<[usize; 3]> = input
@@ -15,46 +16,52 @@ fn run(input: &str, n: usize) -> (usize, usize) {
     let mut distances: BinaryHeap<_> = coords
         .iter()
         .enumerate()
-        .flat_map(|(idx_a, a)| coords.iter().skip(idx_a + 1).map(move |b| (a, b)))
-        .map(|(a, b)| Reverse((distance(a, b) as usize, a, b)))
+        .flat_map(|(idx_a, a)| {
+            coords
+                .iter()
+                .enumerate()
+                .skip(idx_a + 1)
+                .map(move |(idx_b, b)| (a, b, idx_a, idx_b))
+        })
+        .map(|(a, b, idx_a, idx_b)| Reverse((distance(a, b), idx_a, idx_b)))
         .collect();
 
-    let mut clusters: Vec<HashSet<&[usize; 3]>> =
-        coords.iter().map(|c| HashSet::from_iter([c])).collect();
+    let mut clusters: Vec<HashSet<usize, BuildHasherDefault<DumbHash>>> =
+        (0..coords.len()).map(|i| HashSet::from_iter([i])).collect();
 
-    let mut mapping: HashMap<&[usize; 3], usize> =
-        (0..clusters.len()).map(|c| (&coords[c], c)).collect();
+    let mut mapping: HashMap<usize, usize, BuildHasherDefault<DumbHash>> =
+        (0..clusters.len()).map(|c| (c, c)).collect();
 
-    let mut empty = HashSet::<usize>::default();
+    let mut empty = HashSet::<usize, BuildHasherDefault<DumbHash>>::default();
 
     let mut p1 = 0;
 
     let mut i = 0;
-    while let Some(Reverse((_, coords_a, coords_b))) = distances.pop() {
+    while let Some(Reverse((_, idx_a, idx_b))) = distances.pop() {
         if i == n {
             let mut lengths = clusters.iter().map(|c| c.len()).collect::<Vec<_>>();
             lengths.sort_unstable();
             p1 = lengths.iter().rev().take(3).product();
         }
 
-        if mapping[coords_a] == mapping[coords_b] {
+        if mapping[&idx_a] == mapping[&idx_b] {
             i += 1;
             continue;
         }
 
-        empty.insert(mapping[coords_b]);
+        empty.insert(mapping[&idx_b]);
 
         let [a, b] = clusters
-            .get_disjoint_mut([mapping[coords_a], mapping[coords_b]])
+            .get_disjoint_mut([mapping[&idx_a], mapping[&idx_b]])
             .unwrap();
 
         for foo in b.drain() {
             a.insert(foo);
-            mapping.insert(foo, mapping[coords_a]);
+            mapping.insert(foo, mapping[&idx_a]);
         }
 
         if empty.len() == clusters.len() - 1 {
-            return (p1, coords_a[0] * coords_b[0]);
+            return (p1, coords[idx_a][0] * coords[idx_b][0]);
         }
 
         i += 1;
@@ -63,8 +70,25 @@ fn run(input: &str, n: usize) -> (usize, usize) {
     unreachable!()
 }
 
-fn distance<const N: usize>(a: &[usize; N], b: &[usize; N]) -> f64 {
-    ((0..N).map(|i| a[i].abs_diff(b[i]).pow(2)).sum::<usize>() as f64).sqrt()
+fn distance<const N: usize>(a: &[usize; N], b: &[usize; N]) -> usize {
+    (0..N).map(|i| a[i].abs_diff(b[i]).pow(2)).sum::<usize>()
+}
+
+#[derive(Default)]
+struct DumbHash(usize);
+
+impl Hasher for DumbHash {
+    fn finish(&self) -> u64 {
+        self.0 as u64
+    }
+
+    fn write(&mut self, _: &[u8]) {
+        panic!()
+    }
+
+    fn write_usize(&mut self, i: usize) {
+        self.0 = i
+    }
 }
 
 fn main() {
